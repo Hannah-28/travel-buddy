1af98693a2633b46f0692f55295b3e65653effb0
Samrood Ali <devsamrood@gmail.com>
2020-06-29 12:47:22 +0400

    Add:ui.js file to update ui dynamically

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,13 +1,27 @@
 // global variables
 
+
+// getting inputs
 function submitBtn() {
     //recieving location data.
     const location = document.getElementById('location').value;
     const travelDate = document.getElementById('tripdate').value;
+    const endDate = document.getElementById('end-date');
     //sending location data to requests.js
     Client.getCoordinates(location);
+}
+
+
+//Functions to process data
+function tripLength() {
 
 }
+
+
+
+
+
+
 export {
     submitBtn
 }
---
+++ src/client/js/ui.js
@@ -0,0 +1,1 @@
+// functions to handle ui changes
