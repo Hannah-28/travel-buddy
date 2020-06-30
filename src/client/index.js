ab82bb2a0962e253a39d79f84673c17ac3a3e138
Samrood Ali <devsamrood@gmail.com>
2020-06-30 12:09:45 +0400

    Style:files changed

--- src/client/index.js
+++ src/client/index.js
@@ -10,10 +10,7 @@ submitButton.addEventListener('click', (event) => {
 
 // importing style sheets
 import "./styles/resets.scss";
-import "./styles/base.scss";
-import "./styles/footer.scss";
-import "./styles/form.scss";
-import "./styles/header.scss";
+import './styles/styles.scss'
 
 export {
     submitBtn,
