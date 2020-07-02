a61ad052c9aaa0175cc880a13f4510a803c4b181
Samrood Ali <63902576+SamroodAli@users.noreply.github.com>
2020-07-02 18:40:16 +0400

    Fix-Api keys hidden, env file fix needed

--- src/server/server.js
+++ src/server/server.js
@@ -44,21 +44,23 @@ app.post('/postLocation', async(req, res) => {
     res.send(JSON.stringify({ weatherInfo: projectData.weatherInfo, picture: projectData.pictureData }))
 });
 console.log(process.env.API_KEY)
+
     //calling geoname
 async function getLatLong(location) {
-    const url = `http://api.geonames.org/searchJSON?formatted=true&q=${location}&maxRows=10&lang=es&username=samrood&style=full`;
+    //keys seem to be not working, so i have kept placeholders here. -need to be fixed Asap
+    const url = `http://api.geonames.org/searchJSON?formatted=true&q=${location}&maxRows=10&lang=es&username=${process.env.key}&style=full`;
     const data = await fetch(url);
     return data.json();
 }
 // calling weatherbit to get curent weatherInfo
 async function getCurrentWeatherInfo(lat, long) {
-    const url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${long}&key=7ced570754844545b5ecaf5b7cf625f7`
+    const url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${long}&key=${process.env.key}`
     const data = await fetch(url);
     return data.json();
 }
 //getting picture from pixabay
 async function getPicture(location) {
-    const url = `https://pixabay.com/api/?key=17243686-10b27a8ad9736dc58b0218f0e&q=${location}&category=places&image_type=photo&pretty=true`
+    const url = `https://pixabay.com/api/?key=${process.env.key}&q=${location}&category=places&image_type=photo&pretty=true`
     const data = await fetch(url);
     return data.json();
 }
